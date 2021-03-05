b074f5604eedf1ab625f63aba78d62a6cd1a5851
puge <mail@puge.org>
2021-03-05 18:46:38 +0800

    save

--- dist/static/js/owo.core.js
+++ dist/static/js/owo.core.js
@@ -1,4 +1,4 @@
-// Wed Mar 03 2021 15:19:56 GMT+0800 (GMT+08:00)
+// Fri Mar 05 2021 13:36:59 GMT+0800 (GMT+08:00)
 var owo = {tool: {},state: {},event: {}};
 /* 方法合集 */
 var _owo = {
@@ -485,4 +485,21 @@ _owo.showPage = function() {
 // 执行页面加载完毕方法
 _owo.ready(_owo.showPage)
 
+
+
+// 这是用于代码调试的自动刷新代码，他不应该出现在正式上线版本!
+if ("WebSocket" in window) {
+  // 打开一个 web socket
+  if (!window._owo.ws) window._owo.ws = new WebSocket("ws://" + window.location.host)
+  window._owo.ws.onmessage = function (evt) { 
+    if (evt.data == 'reload') {
+      location.reload()
+    }
+  }
+  window._owo.ws.onclose = function() { 
+    console.info('与服务器断开连接')
+  }
+} else {
+  console.error('浏览器不支持WebSocket')
+}
 
